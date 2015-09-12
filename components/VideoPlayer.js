b6f3ded071e17d7298c293d9b2e7d31af953d742
Jackson Morgan <jmorgan45@gatech.edu>
2015-09-12 09:09:39 -0400

    Minor style updates for video player

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -4,8 +4,8 @@ var VideoPlayer = React.createClass({
 	render: function() {
         console.log(this.props.episode);
 		return (
-			<div>
-				<video width="100%" height="100%" controls>
+			<div className="video-container">
+				<video width="100%" height="100%" controls autoplay className="video-player">
 					<source src={this.props.episode.url} type="video/mp4" />
 				</video>
 			</div>
